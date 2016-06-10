916c9bc8fe5058b9c61876b1ecd91c1532fec743
KILL3RTACO <kill3rtaco@gmail.com>
2016-06-09 18:23:59 -0700

    Add Enum to public utilities

---
+++ Enum.js
@@ -0,0 +1,56 @@
+(function() {
+  var Enum,
+    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    hasProp = {}.hasOwnProperty;
+
+  module.exports = Enum = (function() {
+    function Enum() {
+      this.__values = [];
+    }
+
+    Enum.prototype.__addValue = function(name, value) {
+      if (typeof this[name] !== "undefined") {
+        throw new Error(name + " has already been defined");
+      }
+      this[name] = value;
+      return this.__values.push(value);
+    };
+
+    Enum.prototype.values = function() {
+      return this.__values.slice(0);
+    };
+
+    return Enum;
+
+  })();
+
+  Enum.GenericEntry = (function() {
+    function _Class(__name) {
+      this.__name = __name;
+    }
+
+    _Class.prototype.getName = function() {
+      return this.__name;
+    };
+
+    return _Class;
+
+  })();
+
+  Enum.GenericIdEntry = (function(superClass) {
+    extend(_Class, superClass);
+
+    function _Class(__id, __name) {
+      this.__id = __id;
+      this.__name = __name;
+    }
+
+    _Class.prototype.getId = function() {
+      return this.__id;
+    };
+
+    return _Class;
+
+  })(Enum.GenericEntry);
+
+}).call(this);
---
+++ src/Enum.coffee
@@ -0,0 +1,20 @@
+module.exports = class Enum
+
+  constructor: ->
+    @__values = []
+
+  __addValue: (name, value) ->
+    throw new Error("#{name} has already been defined") if typeof @[name] isnt "undefined"
+    @[name] = value
+    @__values.push value
+
+  values: -> @__values.slice 0
+
+Enum.GenericEntry = class
+
+    constructor: (@__name) ->
+    getName: -> @__name
+
+Enum.GenericIdEntry = class extends Enum.GenericEntry
+  constructor: (@__id, @__name) ->
+  getId: -> @__id
